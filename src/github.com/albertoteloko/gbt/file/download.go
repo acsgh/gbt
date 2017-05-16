d024dbbbbd1fb7321b7ce360368a7309626f7ff5
Alberto Crespo <albertocresposanchez@gmail.com>
2017-05-16 18:55:34 +0200

    Add download progress

--- src/github.com/albertoteloko/gbt/file/download.go
+++ src/github.com/albertoteloko/gbt/file/download.go
@@ -5,23 +5,53 @@ import (
 	"net/http"
 	"io"
 	"errors"
+	"github.com/albertoteloko/gbt/log"
 )
 
 func DownloadUrl(url string, output string) error {
-	response, err := http.Get(url)
-	if err == nil {
-		defer response.Body.Close()
-		if response.StatusCode == 200 {
-			file, err := os.Create(output)
-			defer file.Close()
+	var err error
+	log.LogTime("Downloaded: "+url, func() {
+		response, err := http.Get(url)
 
-			if err == nil {
-				_, err = io.Copy(file, response.Body)
+		if err == nil {
+			defer response.Body.Close()
+			if response.StatusCode == 200 {
+				file, err := os.Create(output)
+				defer file.Close()
+
+				readerpt := &progressReader{Reader: response.Body, length: response.ContentLength}
+
+				if err == nil {
+					_, err = io.Copy(file, readerpt)
+					log.Debug("Downloaded %v/%v (%.2f %%)", response.ContentLength, response.ContentLength, float64(100))
+				}
+			} else if response.StatusCode == 404 {
+				err = errors.New(response.Status)
 			}
-		} else if response.StatusCode == 404 {
-			err = errors.New(response.Status)
+		}
+	})
+
+	return err
+}
+
+type progressReader struct {
+	io.Reader
+	total              int64
+	length             int64
+	lastPercentageShow float64
+}
+
+func (pt *progressReader) Read(p []byte) (int, error) {
+	n, err := pt.Reader.Read(p)
+	if n > 0 {
+		pt.total += int64(n)
+		percentage := float64(pt.total) / float64(pt.length) * float64(100)
+
+		if percentage-pt.lastPercentageShow >= 5 {
+			log.Debug("Downloaded %v/%v (%.2f %%)", pt.total, pt.length, percentage)
+			pt.lastPercentageShow = percentage
 		}
 	}
 
-	return err
+	return n, err
 }
